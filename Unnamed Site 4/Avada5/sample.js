e4e6f482bf6e1e07065c1ce4d44cceecac532750
rominar04 <zzavara@yahoo.com>
2021-07-11 12:58:32 +0430

    Upload project on git

---
+++ Unnamed Site 4/Avada5/sample.js
@@ -0,0 +1,13 @@
+ $(document).ready(function(){
+$('#responsive-menu').prmenu();
+});
+
+$(document).ready(function(){
+$('#responsive-menu').prmenu({}
+maintain_ratio: true,
+link: '#000000',
+visited: '#000000',
+hover: '#666666',
+active: '#666666',
+current: '#444444');
+});
